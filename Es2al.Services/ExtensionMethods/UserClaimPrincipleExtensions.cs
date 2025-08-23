0463b2774764f7522cf2806cf982ad0810fe6b89
Ammar Gamal <ammarggmh2004@gmail.com>
2025-08-23 15:52:01 +0300

    add ExtensionMethods for ClaimsPrincipal

---
+++ Es2al.Services/ExtensionMethods/UserClaimPrincipleExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es2al.Services.ExtensionMethods
+{
+    public static class UserClaimPrincipleExtensions
+    {
+        public static int GetUserIdAsInt(this ClaimsPrincipal user)
+        {
+            return Int32.Parse(user.FindFirstValue(ClaimTypes.NameIdentifier)!);
+        }
+        public static string GetUserIdAsString(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier)!;
+        }
+    }
+}
